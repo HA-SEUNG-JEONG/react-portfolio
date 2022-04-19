4097be262b97b9b3ef8c270d9d45b84152120076
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2022-04-19 22:51:24 +0900

    Feat: 사이드바 내 소셜 아이콘 배치

---
+++ src/components/Home.jsx
@@ -0,0 +1,7 @@
+import NavBar from "./NavBar";
+
+const Home = () => {
+  return <div className=" w-full h-screen bg-[#0a192f]"></div>;
+};
+
+export default Home;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import { AiFillHome } from "react-icons/ai";
 import { FaBars, FaTimes } from "react-icons/fa";
+import SideBar from "./SideBar";
 
 const NavBar = () => {
+  const [nav, setNav] = useState(false);
+  const navOnClick = () => setNav(!nav);
   return (
-    <div className="text-gray-200 fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f]">
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
       <div>
         <AiFillHome style={{ width: "3.1rem" }} />
       </div>
@@ -15,18 +19,24 @@ const NavBar = () => {
         <li>Contact</li>
       </ul>
 
-      <div className="hidden">
-        <FaBars />
+      <div onClick={navOnClick} className="md:hidden z-10">
+        {!nav ? <FaBars /> : <FaTimes />}
       </div>
 
-      <ul className="hidden">
-        <li>Home</li>
-        <li>About</li>
-        <li>Projects</li>
-        <li>Contact</li>
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
+        }
+      >
+        <li className="py-6 text-4xl">Home</li>
+        <li className="py-6 text-4xl">About</li>
+        <li className="py-6 text-4xl">Projects</li>
+        <li className="py-6 text-4xl">Contact</li>
       </ul>
 
-      <div className="hidden"></div>
+      <SideBar />
     </div>
   );
 };
