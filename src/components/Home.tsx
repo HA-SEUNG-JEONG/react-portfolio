7a835eaa70c9a79c425388f59f1e19498bced9ca
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2022-04-27 15:13:45 +0900

    feat: 타이핑 애니메이션 라이브러리 적용

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,13 +1,18 @@
 import { HiArrowNarrowRight } from "react-icons/hi";
+import TypeIt from "typeit-react";
 
 const Home = () => {
   return (
-    <div id="home" className=" w-full h-screen bg-[#0a192f]">
-      <div className="max-w-[62.5rem] px-8 flex flex-col justify-center h-full ml-20">
-        <p className="font-bold sm:text-7xl text-[#ebecef]">
-          안녕하세요 예비 프론트엔지니어
-        </p>
-        <h1 className="font-bold sm:text-7xl text-[#8892b0]">정하승입니다.</h1>
+    <div id="home" className=" w-full h-screen bg-[#212327]">
+      <div className="max-w-[80.5rem] px-8 flex flex-col justify-center h-full ml-20">
+        <TypeIt options={{ speed: 100, waitUntilVisible: true }}>
+          <p className="font-bold sm:text-7xl text-[#ebecef]">
+            안녕하세요 예비 프론트엔지니어
+          </p>
+          <h1 className="font-bold sm:text-7xl text-[#8892b0]">
+            정하승입니다.
+          </h1>
+        </TypeIt>
         <div>
           <button className="text-white group border-2 px-6 py-3 my-4 flex items-center hover:bg-yellow-600 hover:border-yellow-bg-yellow-600">
             작업 목록 보기
