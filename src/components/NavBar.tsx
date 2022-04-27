9ca126865ae6bbf9670631baa12c1850693a20c2
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2022-04-27 14:25:40 +0900

    feat:NavBar hover 이벤트 적용

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -8,36 +8,38 @@ const NavBar = () => {
   const [nav, setNav] = useState(false);
   const navOnClick = () => setNav(!nav);
   return (
-    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#212327] text-gray-300">
       <div>
         <AiFillHome style={{ width: "3.1rem" }} />
       </div>
 
-      <ul className="hidden md:flex ">
-        <li>
-          <Link to="home" spy={true} smooth={true} duration={500}>
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link to="skills" spy={true} smooth={true} duration={500}>
-            Skill
-          </Link>
-        </li>
-        <li>
-          <Link to="work" spy={true} smooth={true} duration={500}>
-            Work
-          </Link>
-        </li>
-        <li>
-          <Link to="contact" spy={true} smooth={true} duration={500}>
-            Contact
-          </Link>
-        </li>
-      </ul>
+      <nav className="hidden md:flex">
+        <ul className="flex">
+          <li className="hover:scale-125 font-bold hover:duration-300">
+            <Link to="home" spy={true} smooth={true} duration={500}>
+              Home
+            </Link>
+          </li>
+          <li className="hover:scale-125 font-bold hover:duration-300">
+            <Link to="skills" spy={true} smooth={true} duration={500}>
+              Skill
+            </Link>
+          </li>
+          <li className="hover:scale-125 font-bold hover:duration-300">
+            <Link to="work" spy={true} smooth={true} duration={500}>
+              Work
+            </Link>
+          </li>
+          <li className="hover:scale-125 font-bold hover:duration-300">
+            <Link to="contact" spy={true} smooth={true} duration={500}>
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </nav>
 
-      <div onClick={navOnClick} className="md:hidden z-10">
-        {!nav ? <FaBars /> : <FaTimes />}
+      <div onClick={navOnClick} className="md:hidden z-10 cursor-pointer">
+        {!nav ? <FaBars /> : <FaTimes className="hover:opacity-10" />}
       </div>
 
       <ul
@@ -47,12 +49,11 @@ const NavBar = () => {
             : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
         }
       >
-        <li className="py-6 text-4xl">Home</li>
-        <li className="py-6 text-4xl">About</li>
-        <li className="py-6 text-4xl">Projects</li>
-        <li className="py-6 text-4xl">Contact</li>
+        <li className="py-6 text-3xl">Home</li>
+        <li className="py-6 text-3xl">About</li>
+        <li className="py-6 text-3xl">Projects</li>
+        <li className="py-6 text-3xl">Contact</li>
       </ul>
-
       <SideBar />
     </div>
   );
