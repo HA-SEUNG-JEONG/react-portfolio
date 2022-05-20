553c7bdc253e46e1c94d8b01e8e515d7b8bff770
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2022-05-20 12:02:18 +0900

    fix:프로젝트 링크 추가

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -21,7 +21,7 @@ const Work = () => {
                   Image Resize
                 </span>
                 <div className="pt-8 text-center">
-                  <a href="">
+                  <a href="https://gorgeous-liger-93f086.netlify.app/">
                     <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                       Demo
                     </button>
