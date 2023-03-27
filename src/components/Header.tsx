9e99ccd047ab4d85b1da377f83f15136740f6189
HA-SEUNG-JEONG <gktmd652@naver.com>
2023-03-27 09:53:20 +0900

    Style : Nav 바 작업 1차차

---
+++ src/components/Header.tsx
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <header className="bg-pink-300">Header</header>;
+};
+
+export default Header;
---
+++ src/components/Nav.tsx
@@ -0,0 +1,49 @@
+import { BiHomeAlt, BiUser } from "react-icons/bi";
+import { BsChatSquareText, BsClipboardData } from "react-icons/bs";
+
+import { Link } from "react-scroll";
+
+const Nav = () => {
+  return (
+    <nav className="fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50">
+      <div className="container mx-auto">
+        <div className="w-full bg-black/20 h-[96px] background-blur-2xl rounded-full max-w-[460px] mx-auto px-5 flex justify-between items-center text-2xl text-white/50">
+          <Link
+            className="cursor-pointer w-[60px] flex items-center justify-center"
+            to="home"
+            activeClass="active"
+            smooth={true}
+            spy={true}>
+            <BiHomeAlt />
+          </Link>
+          <Link
+            className="cursor-pointer w-[60px] flex items-center justify-center"
+            to="about"
+            activeClass="active"
+            smooth={true}
+            spy={true}>
+            <BiUser />
+          </Link>
+          <Link
+            className="cursor-pointer w-[60px] flex items-center justify-center"
+            to="work"
+            activeClass="active"
+            smooth={true}
+            spy={true}>
+            <BsClipboardData />
+          </Link>
+          <Link
+            className="cursor-pointer w-[60px] flex items-center justify-center"
+            to="contact"
+            activeClass="active"
+            smooth={true}
+            spy={true}>
+            <BsChatSquareText />
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
