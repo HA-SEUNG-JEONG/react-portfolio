ee531f680fd968d68b9a32d5a96dcf21e28e549b
HA-SEUNG-JEONG <gktmd652@naver.com>
2023-03-28 10:40:07 +0900

    Style : About 페이지 1차 작업

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,8 +1,46 @@
+import { useInView } from "react-intersection-observer";
+
+import { motion } from "framer-motion";
+
+import { fadeIn } from "../variant";
+
 const About = () => {
+  const [ref] = useInView({ threshold: 0.5 });
   return (
-    <div id="about" className="section">
-      About
-    </div>
+    <section id="about" className="section" ref={ref}>
+      <div className="container mx-auto">
+        <div className="flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen">
+          <motion.div
+            variants={fadeIn("right", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}></motion.div>
+          <motion.div className="flex-1 bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top"></motion.div>
+          <motion.div
+            variants={fadeIn("left", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1">
+            <h2 className="h2">About Me</h2>
+            <h3 className="h3 mb-4">Front-End Developer</h3>
+            <p className="mb-6">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas,
+              molestiae. Hic explicabo pariatur repellendus amet dicta totam
+              fuga tempore nihil harum voluptate odio aliquid sapiente provident
+              recusandae, animi, perspiciatis veniam.
+            </p>
+            <div className="flex gap-6 lg:gap-x-10 mb-12"></div>
+            <div className="flex gap-x-8 items-center">
+              <button className="btn btn-lg">Contact Me</button>
+              <a href="#" className="text-gradient">
+                My PortFolio
+              </a>
+            </div>
+          </motion.div>
+        </div>
+      </div>
+    </section>
   );
 };
 
