d69419614f5c5db96912d3a1c1f1d49de56f8397
HA-SEUNG-JEONG <gktmd652@naver.com>
2023-03-29 13:46:32 +0900

    Style : Contact 페이지 1차 작업

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,8 +1,48 @@
+import { motion } from "framer-motion";
+import { fadeIn } from "../variant";
+
 const Contact = () => {
   return (
-    <div className="section" id="contact">
-      Contact
-    </div>
+    <section className="py-16 lg:section" id="contact">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row">
+          {/* text */}
+          <motion.div
+            variants={fadeIn("right", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 flex justify-start items-center">
+            <div>
+              <h4 className="text-xl uppercase font-medium mb-2 tracking-wide">
+                Get in Touch
+              </h4>
+            </div>
+          </motion.div>
+          <motion.form
+            variants={fadeIn("left", 0.2)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 border rounded-2xl flex flex-col gap-y-6 pb-24 p-6 items-start ">
+            <input
+              className="bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all"
+              placeholder="Email"
+              type="text"
+            />
+            <input
+              className="bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all"
+              placeholder="Name"
+              type="text"
+            />
+            <textarea
+              className="bg-transparent border-b py-12 outline-none w-full placeholder:text-white focus:border-accent transition-all resize-none mb-12"
+              placeholder="Message"></textarea>
+            <button className="btn btn-lg">Send Message</button>
+          </motion.form>
+        </div>
+      </div>
+    </section>
   );
 };
 
