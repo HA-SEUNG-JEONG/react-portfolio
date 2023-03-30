5c132713678455d5c733b6e95c66ec8fe31f2422
HA-SEUNG-JEONG <gktmd652@naver.com>
2023-03-30 10:30:48 +0900

    Style : margin 값 추가

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -9,7 +9,7 @@ const Banner = () => {
     <section
       className="min-h-[85vh] lg:min-h-[78vh] flex items-center"
       id="home">
-      <div className="container mx-auto">
+      <div className="container mx-auto mt-20">
         <article className="flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12">
           <div className="flex-1 text-center font-secondary lg:text-left">
             <ShowName />
