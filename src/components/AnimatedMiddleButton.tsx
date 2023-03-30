910213427c9c22dd144396dc47af7a8186731c5d
HA-SEUNG-JEONG <gktmd652@naver.com>
2023-03-30 10:30:33 +0900

    Chore : 필요없는 코드 삭제

--- src/components/AnimatedMiddleButton.tsx
+++ src/components/AnimatedMiddleButton.tsx
@@ -8,12 +8,7 @@ const AnimatedMiddleButton = () => {
       initial="hidden"
       whileInView={"show"}
       viewport={{ once: false, amount: 1 }}
-      className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0">
-      <button className="btn btn-lg">Contact</button>
-      <a href="#" className="text-gradient btn-link">
-        My PortFolio
-      </a>
-    </motion.div>
+      className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0"></motion.div>
   );
 };
 
