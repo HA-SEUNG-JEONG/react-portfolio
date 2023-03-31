f30af6748c36adfa1ed4e2f217041824372eb508
HA-SEUNG-JEONG <gktmd652@naver.com>
2023-03-31 14:35:20 +0900

    Style : 소셜 아이콘 사이즈 추가

--- src/components/SocialLink.tsx
+++ src/components/SocialLink.tsx
@@ -10,11 +10,13 @@ const SocialLink = () => {
       whileInView={"show"}
       viewport={{ once: false, amount: 1 }}
       className="flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0">
-      <a href="https://github.com/HA-SEUNG-JEONG">
-        <FaGithub />
+      <a href="https://github.com/HA-SEUNG-JEONG" className="hover:opacity-60">
+        <FaGithub size={36} />
       </a>
-      <a href="https://www.linkedin.com/in/haseung-jeong-50b831229/">
-        <FaLinkedin />
+      <a
+        href="https://www.linkedin.com/in/haseung-jeong-50b831229/"
+        className="hover:opacity-60">
+        <FaLinkedin size={36} />
       </a>
     </motion.div>
   );
