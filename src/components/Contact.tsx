b35775aec63cd49871eed81551e761282f0a301c
HA-SEUNG-JEONG <gktmd652@naver.com>
2023-03-31 14:34:39 +0900

    Feat : Contact 페이지 수정

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -5,6 +5,9 @@ const Contact = () => {
   return (
     <section className="py-16 lg:section" id="contact">
       <div className="container mx-auto">
+        <h3 className="flex justify-center items-center mb-10 font-bold text-[30px]">
+          Contact Me
+        </h3>
         <div className="flex flex-col lg:flex-row">
           {/* text */}
           <motion.div
@@ -12,19 +15,13 @@ const Contact = () => {
             initial="hidden"
             whileInView={"show"}
             viewport={{ once: false, amount: 0.3 }}
-            className="flex-1 flex justify-start items-center">
-            <div>
-              <h4 className="text-xl uppercase font-medium mb-2 tracking-wide">
-                Get in Touch
-              </h4>
-            </div>
-          </motion.div>
+            className="flex justify-center items-center"></motion.div>
           <motion.form
             variants={fadeIn("left", 0.2)}
             initial="hidden"
             whileInView={"show"}
             viewport={{ once: false, amount: 0.3 }}
-            className="flex-1 border rounded-2xl flex flex-col gap-y-6 pb-24 p-6 items-start ">
+            className="flex-1 border rounded-2xl flex flex-col gap-y-6 pb-24 p-6 items-center justify-center">
             <input
               className="bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all"
               placeholder="Email"
@@ -38,7 +35,12 @@ const Contact = () => {
             <textarea
               className="bg-transparent border-b py-12 outline-none w-full placeholder:text-white focus:border-accent transition-all resize-none mb-12"
               placeholder="Message"></textarea>
-            <button className="btn btn-lg">Send Message</button>
+
+            <a
+              href="mailto:gktmd653@gmail.com"
+              className="btn btn-lg flex items-center justify-center">
+              Send Message
+            </a>
           </motion.form>
         </div>
       </div>
