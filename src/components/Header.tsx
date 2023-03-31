9563c68a565ae6c2b62f4d2fcb66e1d93fccc02b
HA-SEUNG-JEONG <gktmd652@naver.com>
2023-03-31 14:33:45 +0900

    Style : Header 스타일 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,20 @@
+import { Link } from "react-scroll";
+
 const Header = () => {
   return (
-    <header className="py-8">
+    <header className="py-4 fixed left-0 right-0 bg-opacity-50 bg-blue-400">
       <div className="container mx-auto">
         <div className="flex justify-between items-center">
-          <span>Header</span>
-          <button className="btn btn-sm">클릭해보세요</button>
+          <Link to="home" smooth={true} duration={500}>
+            <span className="cursor-pointer font-bold text-[30px] hover:opacity-80">
+              Jeong Ha Seung
+            </span>
+          </Link>
+          <Link to="contact" smooth={true} duration={500}>
+            <button className="btn btn-lg">
+              <span className="font-bold">Contact Me</span>
+            </button>
+          </Link>
         </div>
       </div>
     </header>
