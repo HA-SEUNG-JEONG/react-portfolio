9d8c7fbd68f3ce30edb05427b72ba4312d483ab9
HA-SEUNG-JEONG <gktmd652@naver.com>
2023-04-09 16:10:57 +0900

    Fix : API 키 하드코딩

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -18,7 +18,7 @@ const Contact = () => {
         "service_995hi6p",
         "template_u7u0iua",
         formRef.current ?? "",
-        process.env.REACT_APP_YOUR_PUBLIC_KEY
+        "mtmSRDlwWwn2DKpGj"
       );
       toast.success(result.text);
     } catch (error) {
