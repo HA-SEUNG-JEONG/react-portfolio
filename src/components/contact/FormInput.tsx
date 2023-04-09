2ae3994f6c9eadcc5d6abc8259d89b5147e62176
HA-SEUNG-JEONG <gktmd652@naver.com>
2023-04-09 15:59:53 +0900

    Feat : props 추가 - name,message 추가 - input props에 name 추가

--- src/components/contact/FormInput.tsx
+++ src/components/contact/FormInput.tsx
@@ -1,14 +1,17 @@
 export interface FormInputProps {
   placeholder: string;
   type?: string;
+  name?: string;
+  message?: string;
 }
 
-const FormInput = ({ placeholder, type }: FormInputProps) => {
+const FormInput = ({ placeholder, type, name }: FormInputProps) => {
   return (
     <input
       className="bg-transparent border-b py-3 outline-none w-full focus:border-gray-500 transition-all"
       placeholder={placeholder}
       type={type}
+      name={name}
     />
   );
 };
