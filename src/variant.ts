8cf2c3452e3c952f15cdc19566b3ebc3e6d14bd5
HA-SEUNG-JEONG <gktmd652@naver.com>
2023-04-11 18:18:37 +0900

    Refactor : fadeIn 함수 로직 분리

--- src/variant.ts
+++ src/variant.ts
@@ -1,9 +1,39 @@
+const getXAnimation = (direction: string) => {
+  switch (direction) {
+    case "left":
+      return 100;
+    case "right":
+      return -100;
+    default:
+      return 0;
+  }
+};
+
+const getYAnimation = (direction: string) => {
+  switch (direction) {
+    case "up":
+      return 100;
+    case "down":
+      return -100;
+    default:
+      return 0;
+  }
+};
+
+const getAnimationProps = (delay: number) => {
+  return {
+    duration: 1.2,
+    delay,
+    ease: [0.25, 0.25, 0.25, 0.75],
+  };
+};
+
 export const fadeIn = (direction: string, delay: number) => {
   return {
     hidden: {
-      y: direction === "up" ? 100 : direction === "down" ? -100 : 0,
+      x: getXAnimation(direction),
+      y: getYAnimation(direction),
       opacity: 0,
-      x: direction === "left" ? 100 : direction === "right" ? -100 : 0,
     },
     show: {
       y: 0,
@@ -11,9 +41,7 @@ export const fadeIn = (direction: string, delay: number) => {
       opacity: 1,
       transition: {
         type: "tween",
-        duration: 1.2,
-        delay: delay,
-        ease: [0.25, 0.25, 0.25, 0.75],
+        ...getAnimationProps(delay),
       },
     },
   };
