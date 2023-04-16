63d7f84a26ea9557c35e2b1589825fae8a605252
HA-SEUNG-JEONG <gktmd652@naver.com>
2023-04-16 16:42:25 +0900

    Chore : duration 삭제

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,12 +5,12 @@ const Header = () => {
     <header className="py-4 fixed left-0 right-0 bg-opacity-50 bg-blue-400 select-none">
       <div className="container mx-auto">
         <div className="flex justify-between items-center">
-          <Link to="home" smooth={true} duration={500}>
+          <Link to="home" smooth={true}>
             <span className="cursor-pointer font-bold text-[25px]">
               Jeong Ha Seung
             </span>
           </Link>
-          <Link to="contact" smooth={true} duration={500}>
+          <Link to="contact" smooth={true}>
             <button className="btn btn-lg">
               <span className="font-bold">Contact Me</span>
             </button>
