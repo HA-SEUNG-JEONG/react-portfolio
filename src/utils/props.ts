52ea59548809c3065220f0348919ee7cb2f23518
HA-SEUNG-JEONG <gktmd652@naver.com>
2023-06-29 10:50:12 +0900

    Style : 이미지 스타일 변경

--- src/utils/props.ts
+++ src/utils/props.ts
@@ -1,7 +1,7 @@
 export const implementClasName = "text-xl font-bold mb-2";
 export const projectContainerClassName =
   "max-w-lg mb-16 ml-4 leading-[1.8] flex flex-col font-medium";
-export const imgClassName = "w-[190px] h-[170px] mr-4";
+export const imgClassName = "w-[190px] h-[170px] mr-4 object-cover";
 
 export const commonProps = (amount: number) => ({
   initial: "hidden",
