e2f70bc48af43736570117c69891168950ee0ad7
HA-SEUNG-JEONG <gktmd652@naver.com>
2023-07-01 12:54:14 +0900

    Delete : 필요없는 코드 삭제

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -3,14 +3,13 @@ import TypingIntroduction from "./TypingIntroduction";
 const Introduction = () => {
   return (
     <>
-      <div className="mb-6 text-[27px] lg:text-[40px] font-secondary font-semibold leading-[1.4]">
+      <section className="mb-6 text-[27px] md:w-96 lg:text-[40px] font-secondary font-semibold leading-[1.4]">
         <span className="mr-4">
           유저경험을 중시하는
           <br />
         </span>
         <TypingIntroduction />
-      </div>
-      <p className="mb-8 max-w-lg mx-auto lg:mx-0"></p>
+      </section>
     </>
   );
 };
