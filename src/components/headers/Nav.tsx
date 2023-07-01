6ac152186dac453055fcdd2ba48182f7d4805f1e
HA-SEUNG-JEONG <gktmd652@naver.com>
2023-07-01 14:55:46 +0900

    Style : 배경색 수정

--- src/components/headers/Nav.tsx
+++ src/components/headers/Nav.tsx
@@ -7,7 +7,7 @@ const Nav = () => {
   return (
     <nav className="fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50">
       <section className="container mx-auto">
-        <article className="w-full bg-black/20 h-[96px] background-blur-2xl rounded-full max-w-[460px] mx-auto px-5 flex justify-between items-center text-2xl text-white/50">
+        <article className="w-full bg-black/40 h-[96px] background-blur-2xl rounded-full max-w-[460px] mx-auto px-5 flex justify-between items-center text-2xl text-white/50">
           <HomeIcon />
           <SkillIcon />
           <ProjectIcon />
