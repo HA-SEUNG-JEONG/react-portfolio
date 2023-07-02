13fa90b36fefa108f9ba8fbafbc47c3f3fe90e89
HA-SEUNG-JEONG <gktmd652@naver.com>
2023-07-02 13:00:06 +0900

    Feat : 소셜아이콘에 aria-label 추가

--- src/components/SocialLink.tsx
+++ src/components/SocialLink.tsx
@@ -5,12 +5,14 @@ const SocialLink = () => {
     <div className="flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0">
       <a
         href="https://github.com/HA-SEUNG-JEONG"
-        className="hover:opacity-60 text-gray-900">
+        className="hover:opacity-60 text-gray-900"
+        aria-label="Github">
         <FaGithub size={36} />
       </a>
       <a
         href="https://www.linkedin.com/in/haseung-jeong-50b831229/"
-        className="hover:opacity-60 text-[#0077b5]">
+        className="hover:opacity-60 text-[#0077b5]"
+        aria-label="LinkedIn">
         <FaLinkedin size={36} />
       </a>
     </div>
