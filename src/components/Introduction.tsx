3f520bd3e9a555d1fb41873fa20b5f57645edac3
HA-SEUNG-JEONG <gktmd652@naver.com>
2023-07-02 13:05:08 +0900

    Style : text-gray-900 추가

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -4,7 +4,7 @@ const Introduction = () => {
   return (
     <>
       <section className="mb-6 text-[27px] md:w-96 lg:text-[40px] font-secondary font-semibold leading-[1.4]">
-        <span className="mr-4">
+        <span className="mr-4 text-gray-900">
           유저경험을 중시하는
           <br />
         </span>
