a5a434f8282b65e43d4c5d01eae62c5df5b41be0
HA-SEUNG-JEONG <gktmd652@naver.com>
2024-01-24 16:42:54 +0900

    Style : 소개 문구 수정

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -5,20 +5,21 @@ import TypingIntroduction from "./TypingIntroduction";
 import { motion } from "framer-motion";
 
 const Introduction = () => {
-  return (
-    <>
-      <motion.section
-        variants={fadeIn("right", 0.4)}
-        {...commonProps(0.3)}
-        className="mb-6 text-[27px] md:w-96 lg:text-[40px] font-secondary font-semibold leading-[1.4]">
-        <span className="mr-4 text-gray-900">
-          유저경험을 중시하는
-          <br />
-        </span>
-        <TypingIntroduction />
-      </motion.section>
-    </>
-  );
+    return (
+        <>
+            <motion.section
+                variants={fadeIn("right", 0.4)}
+                {...commonProps(0.3)}
+                className="mb-6 text-[27px] md:w-96 lg:text-[40px] font-secondary font-semibold leading-[1.4]"
+            >
+                <span className="mr-4 text-gray-900">
+                    기록을 좋아하는
+                    <br />
+                </span>
+                <TypingIntroduction />
+            </motion.section>
+        </>
+    );
 };
 
 export default Introduction;
