45ac3c37a8393fe58c5c46df71b03727a7cbe95d
HA-SEUNG-JEONG <gktmd652@naver.com>
2024-01-24 16:53:20 +0900

    Style : 소개 문구 width 추가

--- src/components/TypingIntroduction.tsx
+++ src/components/TypingIntroduction.tsx
@@ -1,15 +1,17 @@
 import { TypeAnimation } from "react-type-animation";
 
 const TypingIntroduction = () => {
-  return (
-    <TypeAnimation
-      sequence={["프론트엔드 개발자 정하승입니다.", 2000]}
-      speed={10}
-      className="text-blue-600"
-      wrapper="span"
-      repeat={Infinity}
-    />
-  );
+    return (
+        <div className="w-screen">
+            <TypeAnimation
+                sequence={["프론트엔드 개발자 정하승입니다.", 2000]}
+                speed={10}
+                className="text-blue-600"
+                wrapper="span"
+                repeat={Infinity}
+            />
+        </div>
+    );
 };
 
 export default TypingIntroduction;
